52b7386eb83a91cd96f839c57c5506703975356c
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2021-06-23 15:56:38 +0500

    adding status to projects

--- app/models/task.rb
+++ app/models/task.rb
@@ -18,7 +18,18 @@ class Task < ApplicationRecord
     when 'complete'
       '#198754'
     end
+  end
 
+  def complete?
+    status == 'complete'
+  end
+
+  def in_progress?
+    status == 'in-progress'
+  end
+
+  def not_started?
+    status == 'not-started'
   end
 
 end
